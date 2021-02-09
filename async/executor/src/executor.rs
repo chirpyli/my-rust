9d30f4b198466b18fc0a9460a805707028458cbb
shengli <s_lisheng@163.com>
2021-02-09 16:55:22 +0800

    compile pass.

--- async/executor/src/executor.rs
+++ async/executor/src/executor.rs
@@ -7,18 +7,22 @@ use pin_utils::core_reexport::sync::atomic::AtomicUsize;
 use std::sync::Mutex;
 use std::sync::atomic::Ordering;
 use once_cell::sync::Lazy;
+use futures::FutureExt;
 
+// 这里的状态很粗，更多状态可以参考 async-task库中的实现
 const WOKEN: usize = 0b01;
 const RUNNING: usize = 0b10;
+const TASK: usize = 0b100;
 
 static QUEUE: Lazy<crossbeam::channel::Sender<Arc<Task>>> = Lazy::new(|| {
+    info!("create static queue with lazy.");
     let (s, r) = crossbeam::channel::unbounded::<Arc<Task>>();
     let num_cores = num_cpus::get();
     for i in 0..num_cores {
         let receiver = r.clone();
         std::thread::spawn(move || {
             receiver.iter().for_each(|task| task.run());
-        })
+        });
     }
 
     s
@@ -45,9 +49,12 @@ impl OwnWeaker {
 impl Wake for OwnWeaker {
     // 对应本Executor的实现，唤醒任务就是将本任务发送到任务队列中
     fn wake(&self) {
-        if self.inner.state.fetch_or(WOKEN, Ordering::SeqCst) == 0 {
-            QUEUE.send(self.inner.clone()).unwrap()
-        }
+        // 更新状态
+        self.inner.state.store(WOKEN, Ordering::SeqCst);
+        QUEUE.send(self.inner.clone()).unwrap();
+        // if self.inner.state.fetch_or(WOKEN, Ordering::SeqCst) == 0 {
+        //     QUEUE.send(self.inner.clone()).unwrap()
+        // }
     }
 }
 
@@ -59,15 +66,16 @@ fn create_waker<W: Wake>(wake: W) -> Waker {
 }
 
 fn create_raw_waker<W: Wake>(wake: W) -> RawWaker {
-    RawWaker::new(Box::new(wake) as *const (), &RawWakerVTable::new(
+    let data = Box::new(wake);
+    RawWaker::new(Box::into_raw(data) as *const (), &RawWakerVTable::new(
         |data| unsafe {
             create_raw_waker((&*(data as *const W)).clone())
         },
         |data| unsafe {
-            &*(data as *const W).wake()
+            (&*(data as *const W)).wake()
         },
         |data| unsafe {
-            &*(data as *const W).wake()
+            (&*(data as *const W)).wake()
         },
         |data| unsafe {
             info!("raw waker drop, do nothing, tmp");
@@ -76,36 +84,17 @@ fn create_raw_waker<W: Wake>(wake: W) -> RawWaker {
 }
 
 
-
-
-// pub fn block_on<F: Future>(future: F) -> F::Output {
-//     pin_utils::pin_mut!(future);
-//
-//     let waker = todo!();
-//     let ref mut cx = Context::from_waker(&waker);
-//
-//     loop {
-//         match future.as_mut().poll(cx) {
-//             Poll::Ready(output) => {
-//                 info!("future poll is ready return final result.");
-//                 return output;
-//             },
-//             Poll::Pending => {
-//                 todo!()
-//             }
-//         }
-//     }
-// }
-
 // An owned permission to join on a task (await its termination)
-type JoinHandle<R> = Pin<Box<dyn Future<Output = R>>>;
-
+// type JoinHandle<R> = Pin<Box<dyn Future<Output = R>>>;
+pub struct JoinHandle<R> {
+    inner: Pin<Box<dyn Future<Output = R>>>,
+}
 
 // Spawns a new asynchronous task, returning a JoinHandle for it.
 // Spawning a task enables the task to execute concurrently to other tasks.
 // The spawned task may execute on the current thread, or it may be sent to a different thread to be executed.
 pub fn spawn<F, R>(future: F) -> JoinHandle<R>
-    where F: Future<Output = R> + Send {
+    where F: Future<Output = R> + Send + 'static, R: Send + 'static {
     let (s, r) = oneshot::channel();
     let future = async move { let _ = s.send(future.await);};
 
@@ -116,16 +105,17 @@ pub fn spawn<F, R>(future: F) -> JoinHandle<R>
 
     QUEUE.send(task).unwrap();
 
-
-    Box::pin(async { r.await.unwrap()})
-
+    JoinHandle {
+        inner: Box::pin(async { r.await.unwrap()})
+    }
 }
 
 
-impl<R> Future for JoinHandle<R> {
+impl<R: Send> Future for JoinHandle<R> {
     type Output = R;
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        self.poll(cx)
+        let handle = Pin::into_inner(self);
+        handle.inner.as_mut().poll(cx)
     }
 }
 
@@ -137,18 +127,12 @@ pub struct Task {
 impl Task {
     pub fn run(self: Arc<Task>) {
         self.state.store(RUNNING, Ordering::SeqCst);
-        let waker = create_waker(self.clone());
+        let waker = create_waker(OwnWeaker::new(self.clone()));
         let cx = &mut Context::from_waker(&waker);
-        self.future.try_lock().unwrap().as_mut().poll(cx);
+        let poll = self.future.try_lock().unwrap().as_mut().poll(cx);
+        // 如果没有就绪的话，要更新状态，擦掉RUNNING的状态
+        if poll.is_pending() {
+            self.state.store(TASK, Ordering::SeqCst);
+        }
     }
 }
-
-pub struct Executor {
-
-}
-
-
-pub struct Spawner {
-
-}
-
--- async/executor/src/main.rs
+++ async/executor/src/main.rs
@@ -6,9 +6,33 @@ mod executor;
 
 use custom_futures::TimerFuture;
 
+
 fn main() {
-    futures::executor::block_on(async {
-        let timer = TimerFuture::new(std::time::Duration::from_secs(3));
-        timer.await;
+    simple_logger::SimpleLogger::new().with_level(log::LevelFilter::Info).init().unwrap();
+    futures::executor::block_on( test_run());
+}
+
+async fn test_run() {
+    info!("test my own executor.");
+
+    let h1 = executor::spawn(async {
+        info!("spawn task 1.");
+        let timer = TimerFuture::new(std::time::Duration::new(3, 0));
+        timer.await
     });
+
+    // let h2 = executor::spawn(async {
+    //     info!("spawn task 2.");
+    //     let timer = TimerFuture::new(std::time::Duration::new(10, 0));
+    //     timer.await
+    // });
+    //
+    // let h3 = executor::spawn( async {
+    //     info!("spawn task3.");
+    //     1 + 2
+    // });
+
+    let r1 = h1.await;
+    // let r2 = h2.await;
+    // let r3 = h3.await;
 }
